0001172059f3a028180e272431774360fcc8823f
Ryan Xue <auspbro@gmail.com>
2016-05-09 15:34:45 +0800

    Add python script

---
+++ Python/createTextFile.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'makeTextFile.py -- create text file'
+
+import os
+ls = os.linesep
+
+# get filename
+while True:
+
+	fname = raw_input('Please input filename:')
+	if os.path.exists(fname):
+		print "ERROR: '%s' already exists" % fname
+	else:
+		break
+
+# get file content (text) line
+all = []
+print "\nEnter lines ('.' by itself to quit).\n"
+
+# loop until user terminate input
+while True:
+	entry = raw_input('> ')
+	if entry == '.':
+		break
+	else:
+		all.append(entry)
+
+# Write lines to file with proper line-ending
+fobj = open(fname, 'w')
+fobj.writelines(['%s%s' % (x, ls) for x in all])
+fobj.close()
+print 'DONE!'
+
+		
