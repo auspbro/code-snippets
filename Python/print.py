62e53c320844ae1d391eecc46a62f06719fdb4dc
Ryan Xue <auspbro@gmail.com>
2016-05-09 14:15:56 +0800

    Add Python script

---
+++ Python/print.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python
+#!/usr/bin/python
+#!/usr/bin/env python
+
+print "Hello, World!"
+print 'Hello, Python!'
---
+++ Python/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+#print "Hello, World!"
+#print 'Hello, Python!'
+print 1 + 2 * 4
