3e468970b22bff1841fe2554594e1f27a76c6a4c
Ryan Xue <auspbro@gmail.com>
2016-05-11 17:55:40 +0800

    Add Python script to LPTHW

---
+++ Python/LPTHW/ex5.py
@@ -0,0 +1,17 @@
+
+my_name = 'Julie'
+my_age = 30	# not a lie
+my_height = 172 # cm
+my_weitht = 66 # kg
+my_teeth = 'white'
+my_eyes = 'black'
+my_hair = 'black'
+
+print "Let's talk about %s." % my_name
+print "He is %d cm tall." % my_height 
+print "He is %d kg heavy." % my_weitht
+print "That's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "He's teeth are usualy %s depending on coffee." % my_teeth
+# "This line is tricky, try to get it exactly right"
+print "If i add %d, %d, and %d I get %d." % (my_age, my_weitht, my_height, my_age + my_weitht + my_height)
---
+++ Python/LPTHW/ex7.py
@@ -0,0 +1,23 @@
+
+print "Mary had a little lamp."
+print "It's fleece was white as %s." % 'snow'
+print "And ervery where Mary went."
+print "." * 10	# What'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. Try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 +end12
+
