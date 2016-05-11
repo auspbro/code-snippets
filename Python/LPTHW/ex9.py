c9dd86e2cb04fe9b7ac4eb988c80e7e3496d4e89
Ryan Xue <auspbro@gmail.com>
2016-05-11 19:30:29 +0800

    Add python script to lpthw

---
+++ Python/LPTHW/ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There are something going on here.
+With the three double qurotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want or 5, or 6.
+"""
