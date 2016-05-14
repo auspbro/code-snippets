0660958cc4bebbece686fcc573122aca3fccb62a
Ryan Xue <auspbro@gmail.com>
2016-05-14 19:44:45 +0800

    add new script

---
+++ Python/LPTHW/ex30.py
@@ -0,0 +1,25 @@
+people = 30
+cars = 40 
+buses = 15
+
+if cars > people:
+	print "We should take the car."
+elif cars < people:
+	print "We should not take the cars."
+else:
+	print "We can not decide."
+
+
+if buses > cars:
+	print "That's too many buses"
+elif buses < cars:
+	print "May be we could take the buses."
+else:
+	print "We can not decide."
+
+if people > buses:
+	print "Alright, let's just take the buese. "
+else:
+	print "Fine, let's stay home then."
+
+
