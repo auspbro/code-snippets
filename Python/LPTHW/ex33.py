a987467fdd600e02761e4470296310a5f66650b3
Ryan Xue <auspbro@gmail.com>
2016-05-16 16:32:33 +0800

    Add new scripty

---
+++ Python/LPTHW/ex33.py
@@ -0,0 +1,14 @@
+i = 0
+number = []
+
+while i < 9:
+	print "At the top i is %d" % i
+	number.append(i)
+	i = i + 1
+	print "Number now: ", number
+	print "At the bottom i is %d" % i
+
+print "The Number:"
+
+for num in number:
+	print num
