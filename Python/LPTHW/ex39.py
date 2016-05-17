97212667cabccd08c55c50e06f345a452578bb27
Ryan Xue <auspbro@gmail.com>
2016-05-17 14:36:56 +0800

    Add new script

---
+++ Python/LPTHW/ex39.py
@@ -0,0 +1,17 @@
+class Song(object):
+	"""docstring for Song"""
+	def __init__(self, lyrics):
+		super(Song, self).__init__()
+		self.lyrics = lyrics
+		
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+
+happy_bday = Song(["happy birthday to you", "I don't want to get sued", "So, I'll stop right there"])
+
+bulls_on_parade = Song(["They really around the family", "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
