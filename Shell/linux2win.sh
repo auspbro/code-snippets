20a7856254d57379ef2a01a261e0c10e7d22d2ca
auspbro <auspbro@gmail.com>
2016-12-09 17:22:10 +0800

    add number #2# setting comment

--- Shell/linux2win.sh
+++ Shell/linux2win.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo sed -i 's/set default="0"/set default="2"/g' /boot/grub/grub.cfg
+
+# 首先开机或者重启，在启动项选择菜单处记住Windows OS对应的序号，从上至下的序号从0开始计数，我的Win10系统选项处于第3个，那么序号就应该是2，记住后，打开ubuntu系统。
+sudo sed -i 's/set default="0"/set default="2"/g' /boot/grub/grub.cfg  # 数字"2"取决于启动项选择菜单处Windows那一列对应的序号
 sudo reboot
