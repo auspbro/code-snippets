88cca3dc7fd0ae295334db59a376b9ad64dfad1e
auspbro <auspbro@gmail.com>
2016-12-11 18:38:47 +0800

    First commit

---
+++ Shell/AMD_Diag_Go.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Check AMD Diagnostics pass/fail log script.
+# Copyright (c) 2016 Free Software Foundation, Inc.
+# 
+###################################################
+
+
+cd /home/diag/Desktop/Baffin_B-PROD_16-07-03_LINUX-64
+sudo bash ./Diag
+
+sleep 5
+
+
+
+
+
