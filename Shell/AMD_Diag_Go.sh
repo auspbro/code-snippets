31b8d3858892f30406dc38fba7f6eab6dfa98a85
auspbro <auspbro@gmail.com>
2016-12-13 19:03:25 +0800

    add new script

--- Shell/AMD_Diag_Go.sh
+++ Shell/AMD_Diag_Go.sh
@@ -1,15 +1,37 @@
-#! /bin/sh
+#! /bin/bash
 
+###################################################
 # Check AMD Diagnostics pass/fail log script.
 # Copyright (c) 2016 Free Software Foundation, Inc.
-# 
-###################################################
+##################################################
+# Name: AMD_Diag_Go.sh
+# Description: Check AMD Diagnostics pass/fail log script.
+# History:
+# 2016/12/13	Capt.	First release.
+##################################################
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+export PATH
+##################################################
+# Variables
+#
+user=$LOGNAME
+password="password"
+logdir="/home/diag/Desktop/Baffin_B-PROD_16-07-03_LINUX-64/"
+tmpmountpoint="/mnt/temp_mount"
+dstdir="/blah/backups/mysql_backups/"
+##################################################
+# Usage Options
 
-
-cd /home/diag/Desktop/Baffin_B-PROD_16-07-03_LINUX-64
+echo "Now, I will run AMD Diagnostics..."
+echo -e "The Diagnostics test log will be generated on $logdir"
+cd $logdir
 sudo bash ./Diag
-
 sleep 5
+
+grep -q "Total Fail:	0"  $logdir/log.txt
+
+
+exit 0
 
 
 
