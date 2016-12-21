9911900f3eb9d8eb3e2a73bd06623e1bfcb737ab
Ryan Xue <auspbro@gmail.com>
2016-12-21 23:47:04 +0800

    add new feature

--- Shell/ABS_Guide/clean_log.sh
+++ Shell/ABS_Guide/clean_log.sh
@@ -17,9 +17,11 @@ export PATH
 user=$LOGNAME
 password="password"
 scriptdir="/home/diag/Desktop/"
-LOGDIR=/var/log
+LOG_DIR=/var/log
 ROOT_UID=0		# $UID为0的时候，用户才具有根权限
 LINES=50		# 默认保存行数
+E_XCD=66		# 不能修改目录？
+E_NOTROOT=67	# 非根用户将以error退出
 
 tmpmountpoint="/mnt/temp_mount"
 dstdir="/blah/backups/mysql_backups/"
@@ -28,8 +30,24 @@ dstdir="/blah/backups/mysql_backups/"
 
 
 ###### START SCRIPT ########
-cd /var/log
+if [ "$UID" -ne "$ROOT_UID" ]; then	
+	echo "Must be root to run this script."
+	exit $E_NOTROOT
+fi
+
+if [[ -n "$1" ]]; then
+	lines=$1
+else
+	lines=$LINES 	# 		
+fi
+
+cd $LOG_DIR
+if [[ `pwd` != "$LOG_DIR" ]]; then
+	echo "Can't change to $LOG_DIR."
+	exit $E_XCD
+fi
+
 cat /dev/null > message
 cat /dev/null > wtmp
 echo "Logs cleaned up..."
-exit
+exit 0
