85fc237312c3b91b2ace8706b257867bd83d3f83
auspbro <auspbro@gmail.com>
2017-01-05 12:45:58 +0800

    Add Physics RAM size check feature.

--- Shell/AMD_Diag_Go.sh
+++ Shell/AMD_Diag_Go.sh
@@ -8,6 +8,7 @@
 # Description: Check AMD Diagnostics pass/fail log script.
 # History:
 # 2016/12/13	Capt.	First release.
+# 2016/01/05	Capt.	Add Physics RAM size check feature.
 ##################################################
 PATH = /usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 export PATH
@@ -22,7 +23,32 @@ tmpmountpoint="/mnt/temp_mount"
 dstdir="/blah/backups/mysql_backups/"
 ##################################################
 # Usage Options
+#
+# usage () {
+#   echo "Usage: $0 [OPTION]"
+#   echo "-d, --dest=name       Destination directory. Default is /tmp"
+#   echo "-h, --help            Display this help and exit."
+#   echo "-p, --password[=name] Password to use when connecting to server. If password is"
+#   echo "                      not given it's asked from the tty."
+#   echo "-t                    Temporary mount point for the snapshot. Default is /mnt."
+#   echo "-u, --user=name       User for login if not current user"
+#   exit 1
+# }
 
+##################################################
+# Physics Memory Size Check
+# If RAM size > 4GB switch to Windows
+#
+sudo dmidecode --type memory > $logdir/ramsize.log
+grep -q "Maximum Capacity: 4 GB" $logdir/ramsize.log
+if [ "$?" -eq "0" ]; then
+	sudo bash $logdir/switch2win.sh
+fi
+
+##################################################
+# Start AMD Diagnostics Test
+# And check the log file pass or fail.
+#
 echo "Now, I will run AMD Diagnostics..."
 echo -e "The Diagnostics test log will be generated on $logdir"
 cd $logdir
@@ -31,9 +57,9 @@ sleep 5
 
 grep -q "Total Fail:	0"  $logdir/log.txt
 if [ "$?" -eq "0" ]; then
-	sudo bash $scriptdir/switch2win.sh
+	sudo bash $logdir/switch2win.sh
 else
-	echo "Please call 911..."
+	echo "AMD Diagnostics Test Failed, Please call 911...@@"
 fi
 
 exit 0
