939ab6f901cc62759269d97cb49dda85111489c9
auspbro <auspbro@gmail.com>
2017-12-28 10:53:36 +0800

    Update showPic.js

--- JavaScript/image-gallery/js/showPic.js
+++ JavaScript/image-gallery/js/showPic.js
@@ -19,12 +19,15 @@ function popUp(winURL) {
     window.open(winURL, "popup", "width=320, height=480")
 }
 
-var links = document.getElementsByTagName("a")
-for (let i = 0; i < links.length; i++) {
-    if (links[i].getAttribute("classs") == "popup") {
-        links[i].onclick = function () {
-            popUp(this.getAttribute("href"))
-            return false
+window.onload = prepareLinks
+function prepareLinks() {
+    var links = document.getElementsByTagName("a")
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].getAttribute("classs") == "popup") {
+            links[i].onclick = function () {
+                popUp(this.getAttribute("href"))
+                return false
+            }
         }
-    }    
+    }
 }
