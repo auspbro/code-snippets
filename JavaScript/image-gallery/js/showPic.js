d91c49e849fca2c717388569cd674866c4695d3f
auspbro <auspbro@gmail.com>
2017-12-29 14:45:56 +0800

    Fine tune showPic.js

--- JavaScript/image-gallery/js/showPic.js
+++ JavaScript/image-gallery/js/showPic.js
@@ -34,3 +34,32 @@ function prepareLinks() {
         }
     }
 }
+
+
+function prepareGallery() {
+    if (!document.getElementsByTagName || !document.getElementById || !document.getElementById("imagegallery")) {
+        return false
+    }
+    var gallery = document.getElementById("imagegallery")
+    var links = gallery.getElementsByTagName("a")
+    for (let i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            showPic(this)
+            return false
+        }        
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload
+    if (typeof window.onload != 'function') {
+        window.onload = func        
+    } else {
+        window.onload = function () {
+            oldonload()
+            func()
+        }
+    }
+}
+
+addLoadEvent(prepareGallery)
