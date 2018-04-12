bd7646124af067e56130b1c8120ac27b578ec7cd
auspbro <auspbro@gmail.com>
2018-04-12 15:00:16 +0800

    fine tune...

--- Python/LPTHW/ex8.py
+++ Python/LPTHW/ex8.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 
 formatter = "%r %r %r %r"
 formatter2 = "%s %s %s %s"
 
 print formatter % (1, 2, 3, 4)
-print formatter % ("one", "two", "three", "four")
+print formatter % (u"one你好", "two", "three", "four")
 print formatter % (True, False, False, True)
 print formatter % (formatter, formatter, formatter, formatter)
 print formatter % ("I had this thing.", "That you could type up right.", "But it didn't sing.", 'So I said goodnight.')
 
 print formatter2 % (1, 2, 3, 4)
-print formatter2 % ("one", "two", "three", "four")
+print formatter2 % (u"one你好", "two", "three", "four")
 print formatter2 % (True, False, False, True)
 print formatter2 % (formatter, formatter, formatter, formatter)
 print formatter2 % (
@@ -18,3 +22,6 @@ print formatter2 % (
 	"But it didn't sing.", 
 	"So I said goodnight."
 	)
+
+
+print u"中华人民共和国"
