c8fa173639eed6814b15187899e92673675a50d6
auspbro <auspbro@gmail.com>
2018-04-19 20:14:12 +0800

    Code review

---
+++ Python/LPTHW/ex19_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def mycomputer(*args):
+    arg1, arg2, arg3, arg4 = args
+    print "HDD size: %d" % arg1
+    print "memory size: %d" % arg2
+    print "LCD type: %s" % arg3
+    print "CPU type: %s" % arg4
+    print "Get a blanket.\n"
+
+
+print "Show me the computer components:"
+mycomputer(128, 8, 'THD', 'I7')
+
+print "Show me the computer components2:"
+hdd_size = 500
+ram_size = 8
+mycomputer(hdd_size, ram_size, 'ibk', 'i9')
--- Python/LPTHW/ex20.py
+++ Python/LPTHW/ex20.py
@@ -3,13 +3,13 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-	f.seek(0)
+	print f.read()
 
 def rewind(f):
 	f.seek(0)
 
 def print_a_line(line_count, f):
-	print line_count, f.readline()
+	print line_count, f.readline(),
 
 current_file = open(input_file)
 
