0738be758ae3e2cbb922f621c7b332af1b6bb975
Ryan Xue <auspbro@gmail.com>
2018-05-12 23:56:47 +0800

    Update script

--- Python/LPTHW/ex33.py
+++ Python/LPTHW/ex33.py
@@ -1,14 +1,25 @@
+
+# while i < 9:
+# 	print "At the top i is %d" % i
+# 	number.append(i)
+# 	i = i + 1
+# 	print "Number now: ", number
+# 	print "At the bottom i is %d" % i
+
+# print "The Number:"
+
+# for num in number:
+# 	print num
+
 i = 0
 number = []
+counts = 9
+def list_loop(counts):
+	global i
+	while i < counts:
+		number.append(i)
+		i += 1
+		print "Number now: ", number
 
-while i < 9:
-	print "At the top i is %d" % i
-	number.append(i)
-	i = i + 1
-	print "Number now: ", number
-	print "At the bottom i is %d" % i
-
-print "The Number:"
-
-for num in number:
-	print num
+new_num = list_loop(9)
+print "The numbers: ", new_num
