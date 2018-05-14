85d9e99ec822272e2a3c9c11f8240e78fd9f7113
Ryan Xue <auspbro@gmail.com>
2018-05-14 23:44:01 +0800

    Update script

--- Python/LPTHW/ex39.py
+++ Python/LPTHW/ex39.py
@@ -1,16 +1,22 @@
 class Song(object):
-	"""docstring for Song"""
-	def __init__(self, lyrics):
-		super(Song, self).__init__()
-		self.lyrics = lyrics
-		
-	def sing_me_a_song(self):
-		for line in self.lyrics:
-			print line
+    """docstring for Song"""
 
-happy_bday = Song(["happy birthday to you", "I don't want to get sued", "So, I'll stop right there"])
+    def __init__(self, lyrics):
+        super(Song, self).__init__()
+        self.lyrics = lyrics
 
-bulls_on_parade = Song(["They really around the family", "With pockets full of shells"])
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+
+happy_bday = Song([
+    "happy birthday to you", "I don't want to get sued",
+    "So, I'll stop right there"
+])
+
+bulls_on_parade = Song(
+    ["They really around the family", "With pockets full of shells"])
 
 happy_bday.sing_me_a_song()
 
