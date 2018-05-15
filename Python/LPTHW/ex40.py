15f23e413e6eff84b433289767bebe240730df1c
Ryan Xue <auspbro@gmail.com>
2018-05-15 21:32:14 +0800

    Update script

--- Python/LPTHW/ex40.py
+++ Python/LPTHW/ex40.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-#coding:utf8
+# coding: utf8
+
 
 class Song(object):
     def __init__(self, lyrics):
@@ -9,7 +10,12 @@ class Song(object):
         for line in self.lyrics:
             print line
 
-happy_bday = Song(["Happy birthday to you", "I don't want to get sued", "So I'll stop right there"])
-bull_on_parada = Song(["They rally around the family", "With pockets full of shells"])
+
+happy_bday = Song([
+    "Happy birthday to you", "I don't want to get sued",
+    "So I'll stop right there"
+])
+bull_on_parada = Song(
+    ["They rally around the family", "With pockets full of shells"])
 happy_bday.sing_me_a_song()
 bull_on_parada.sing_me_a_song()
