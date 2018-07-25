d7e47e3c655a5c09ede19f730b1ce968a5ae96e5
Xiang Xue <auspbro@gmail.com>
2018-07-25 15:44:53 +0800

    Add new script

---
+++ Python/LPTHW/ex45.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# coding:utf-8
+# History:
+
+'''
+description:
+'''
+
+__author__ = 'Xiang Xue'
+__version__ = '1.0'
+
+
+class Animal(object):
+    pass
+
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Person(object):
+    def __init__(self,name):
+        self.name = name
+        self.pet = None
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        super(Employee,self).__init__(name)
+        self.salary = salary
+
+class Fish(object):
+    pass
+
+class Salmon(Fish):
+    pass
+
+class Halibut(Fish):
+    pass
+
+rover = Dog("Rover")
+
+satan = Cat("Satan")
+
+mary = Person("Mary")
+
+mary.pet = satan
+
+mary.pet = satan
+
+frank = Employee("Frank", 12000)
+
+frank.pet = rover
+
+flipper = Fish()
+
+crouse = Salmon()
+
+harry = Halibut()
