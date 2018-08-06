79895f6807cc9c1eb99a6927c060f284ef5b1f1d
Xiang Xue <auspbro@gmail.com>
2018-08-06 14:25:46 +0800

    Add new script

---
+++ Python/pycode_LXF/my_abs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+        
