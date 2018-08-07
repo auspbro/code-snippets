1e4ebea48b68c6795a3a2461e38362a22174c3ad
Xiang Xue <auspbro@gmail.com>
2018-08-07 16:04:10 +0800

    Add new script

--- Python/pycode_LXF/calc.py
+++ Python/pycode_LXF/calc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def calc(parameter_list):
+def calc(*parameter_list):
     sum = 0
     for n in parameter_list:
         sum = sum + n * n
@@ -15,5 +15,5 @@ print u"请输入参数2:",
 p2 = int(raw_input())
 print u"请输入参数3:",
 p3 = int(raw_input())
-print u"%d*%d+%d*%d+%d*%d平方和结果是：%d" % (p1, p1, p2, p2, p3, p3, calc([1, 2, 3]))
+print u"%d*%d+%d*%d+%d*%d平方和结果是：%d" % (p1, p1, p2, p2, p3, p3, calc(1, 2, 3))
 # print u"平方和结果是：%d" % calc([1, 3, 5, 7])
