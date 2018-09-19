68c5176d24317869e329a4e9139f6f8d5a42d595
Ryan Xue <auspbro@gmail.com>
2018-09-19 10:39:59 +0800

    Add new python script

---
+++ Python/pycode_LXF/fab_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+使用递归方法
+'''
+
+def fab(n):
+    if n < 1:
+        print('输入有误!')
+        return -1
+
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fab(n-1) + fab(n-2)
+
+result = fab(40)
+if result != -1:
+    print('总共有%d对小兔崽子诞生!' % result) 
