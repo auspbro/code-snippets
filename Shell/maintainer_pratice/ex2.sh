c753925985db2f6072e2511231c4a7a0c7ce1286
Ryan Xue <auspbro@gmail.com>
2018-10-22 14:42:45 +0800

    first commit

---
+++ Shell/maintainer_pratice/ex2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WORK_DIR=/oldboy/
+
+create(){
+    i=1
+    while(($i<11))
+    do
+        cd $WORK_DIR && touch `tr -dc "a-z"</dev/urandom | head -c 10`_oldboy.html
+        i=$(($i+1))
+    done
+}
+
+check(){
+    if [ -d $WORK_DIR ];
+    then
+        create
+    else
+        mkdir $WORK_DIR
+        create
+    fi
+}
+
+check
